e1828be41767e6937a7db5ba60a0f011f36fc76f
M-Beyer <23377889+M-Beyer@users.noreply.github.com>
2019-03-06 20:05:47 +0100

    Minor formatting changes and renaming

--- Exercises/Exercise_9/client.py
+++ Exercises/Exercise_9/client.py
@@ -2,36 +2,37 @@
 #  Exercise 9: Scripting languages
 #  Part 2: Client side scripting
 #  Programming Languages 2
-#  Michael Beyer / Giacomo Bossi 
+#  Michael Beyer, Giacomo Bossi
 #
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
-serverAddress = "https://courses.softlab.ntua.gr/pl2/2018b/exercises/palseq.php"
-serverAddress2 = "http://localhost:3000"
+# Choose server:
+#serverAddress = "https://courses.softlab.ntua.gr/pl2/2018b/exercises/palseq.php"
+serverAddress = "http://localhost:3000"
 
-
+playCount = 30
 
 
 def longestPalSubstr(string):
     n = len(string)
 
-    L = [[0 for x in range(n)]for y in range(n)] 
+    L = [[0 for x in range(n)]for y in range(n)]
 
-    for i in range(n): 
+    for i in range(n):
         L[i][i] = 1
 
-    for cl in range( 2, n+1): 
-        for i in range(n - cl + 1): 
+    for cl in range( 2, n+1):
+        for i in range(n - cl + 1):
             j = i + cl - 1
-            if (string[i] == string[j] and cl == 2): 
+            if (string[i] == string[j] and cl == 2):
                 L[i][j] = 2
-            elif (string[i] == string[j]): 
+            elif (string[i] == string[j]):
                 L[i][j] = L[i + 1][j - 1] + 2
-            else: 
-                L[i][j] = max(L[i][j - 1],L[i + 1][j]) 
+            else:
+                L[i][j] = max(L[i][j - 1],L[i + 1][j])
 
     return (L[0][n - 1])
 
@@ -48,28 +49,30 @@ def play():
     form = driver.find_element_by_id( "answer" )
     form.send_keys( str(len(currentQuestion.text) - res ) + Keys.RETURN )
 
-    print(driver.find_element_by_xpath( "//p[@class]" ).text)
+    print( driver.find_element_by_xpath( "//p[@class]" ).text )
 
 
 # Setup
 chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument( '--headless' )
+chrome_options.add_argument( '--no-sandbox' )
 
 driver = webdriver.Chrome( chrome_options=chrome_options )
-driver.get( serverAddress2 )
+driver.get( serverAddress )
 
-count = 0
-while count < 10:
+print( "Playing for {0} rounds!\n".format( playCount ) )
+
+while playCount:
     play()
 
     # check for next round
     if( len( driver.find_elements_by_id( "again" ) ) != 0 ):
         driver.find_element_by_id( "again" ).click()
     else:
-        print( "Could't find 'again' button. Exiting..." )
+        print( "Could't find 'again' button!" )
         break
 
-    count += 1
+    playCount -= 1
 
+print( "Exiting..." )
 driver.close();
