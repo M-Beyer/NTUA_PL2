764ececcc6962fe8504c4ac15e7b7a71e249f194
M-Beyer <23377889+M-Beyer@users.noreply.github.com>
2019-03-06 20:06:27 +0100

    Formatting, game is now restarted on reload

--- Exercises/Exercise_9/server.js
+++ Exercises/Exercise_9/server.js
@@ -2,23 +2,23 @@
 //  Exercise 9: Scripting languages
 //  Part 1: Server side scripting
 //  Programming Languages 2
-//  Michael Beyer
+//  Michael Beyer, Giacomo Bossi
 //
 
 //
 // ! npm install express and pug !
 //
 
-var path       = require('path');
-var express    = require('express');
-var bodyParser = require("body-parser");
-var game       = require('./game');
+var path       = require( 'path' );
+var express    = require( 'express' );
+var bodyParser = require( "body-parser" );
+var game       = require( './game' );
 
 var app = express();
 
 // view engine setup
-app.set('views', 'views');
-app.set('view engine', 'pug');
+app.set( 'views', 'views' );
+app.set( 'view engine', 'pug' );
 app.locals.pretty = true;       // generate pretty html
 
 var PORT = 3000;
@@ -28,30 +28,33 @@ app.use(bodyParser.urlencoded( { extended: true } ) );
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // GET requests
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-app.get('/', function( req, res )
+app.get( '/', function( req, res )
 {
-  let gameValues = game.getCurrent();
-  gameValues.state = '';
+  let gameValues = game.restart();
   res.render( 'index', gameValues );
 });
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // POST requests
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-app.post('/pl2/longestPal', function( req, res )
+app.post( '/pl2/longestPal', function( req, res )
 {
-  let state = '';
   let gameValues = game.getCurrent();
-  gameValues.state = state;
 
-  if( req.body.hasOwnProperty('answer') )
+  if( req.body.hasOwnProperty( 'answer' ) )
   {
-    state = game.checkAnswer(req.body['answer']);
-    gameValues.state = state;
+    gameValues.answerState = game.checkAnswer( req.body['answer'] );
   }
 
-  res.render('index', gameValues );
+  if( req.body.hasOwnProperty( 'restart' ) )
+  {
+    gameValues = game.restart();
+  }
+
+  res.render( 'index', gameValues );
+
+  game.clearAnswerState();
 });
 
-app.listen(PORT);
+app.listen( PORT );
 console.log( 'Listening on localhost, port ' + PORT );
